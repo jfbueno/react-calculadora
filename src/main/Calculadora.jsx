4bbf8ebe3ac5da8e888793cb207788e4ff0663b5
Jéferson <jeferson.bueno@cwi.com.br>
2020-02-13 17:54:18 -0300

    Implementação de operações

--- src/main/Calculadora.jsx
+++ src/main/Calculadora.jsx
@@ -5,12 +5,22 @@ import Display from '../components/Display'
 
 export default class Calculadora extends Component
 {
+
+    clearMemory = () => console.log('Limpar')
+    
+    setOperation = operation => console.log(operation)
+    
+    addDigit = digit => console.log(digit)
+
     render() {
+        const addDigit = n => this.addDigit(n)
+        const setOperation = op => this.setOperation(op)
+        
         return (
             <div className="calculator">
                 <Display value={1000}></Display>
-                <Button label="AC"></Button>
-                <Button label="/"></Button>
+                <Button label="AC" click={() => this.clearMemory()}></Button>
+                <Button label="/" click={setOperation}></Button>
                 <Button label="7"></Button>
                 <Button label="8"></Button>
                 <Button label="9"></Button>
